91b7b29c9f80840c75846bde895c711627a415c9
Walid Douri <walid_douri@hotmail.com>
2022-03-05 23:57:37 -0500

    File Clean up and Landing page

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import Landing from'./pages/Landing'
+// import Header from'./pages/Header'
+// import Footer from'./pages/Footer'
+// import Dashboard from'./pages/Dashboard'
+
+function App() {
+  return (
+    <div>
+        <Landing/>
+      {/* <Header/>
+      <Footer/> */}
+    </div>
+    
+  );
+}
+
+export default App;
